1012d772d67df3b56d48987ecc1bbb752d727ab2
morgancollado <Morgan.collado@gmail.com>
2020-11-27 20:05:43 -0600

    commit tree b17e4e99350e51a9920c933af565bf8198675c04

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(number => number *= -1)
+}
+
+function mapToNoChange(sourceArray){ 
+    return sourceArray.map(number => number = number)
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(number => number * 2)
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(number => number ** 2)
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    return sourceArray.reduce(function(total, current){
+        return total + current
+    }, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray){
+    return sourceArray.reduce(function(result, value){
+        return result && !!value;
+    }, true);
+};
+
+function reduceToAnyTrue(arr) {
+    return arr.reduce(function(result, value) {
+        return result || !!value;
+    }, false);
+};
